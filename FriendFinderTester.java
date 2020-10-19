2ed2a0325d9481cfb2a2e2697396eeaac3b922bf
tnaz84 <71796629+tnaz84@users.noreply.github.com>
2020-10-19 09:24:57 -0500

    Add files via upload

---
+++ FriendFinderTester.java
@@ -0,0 +1,64 @@
+// --== CS400 File Header Information ==--
+// Name: Tricia Nazareth
+// Email: tnazareth@wisc.edu
+// Team: NE
+// TA: Daniel Finer
+// Lecturer: Florian
+// Notes to Grader: <optional extra notes>
+
+public class FriendFinderTester{
+
+  /**
+   * Tests that FriendFinderTree cannot print out an empty tree
+   */
+  public static boolean testIsEmpty() {
+    FriendFinderRBTree test1 = new FriendFinderRBTree;
+    boolean result = test1.isEmpty();
+    if (result != true) {
+    	return false; 
+    }
+    return true;
+  }
+  
+  /**
+   * Tests the contains method to check if the inserted friends are part of the tree
+   */
+  public static boolean testContains() {
+    return false;
+  }
+  
+  /**
+   * Tests the PrintAllInOrder method to check if the correct order of the tree is 
+   * printed out.
+   */
+  public static boolean testPrintAllInOrder() {
+    return false;
+  }
+  
+  /**
+   * Tests the PrintAllLevelOrder method to check ...
+   */
+  public static boolean testPrintAllLevelOrder() {
+    return false;
+  }
+  
+  /**
+   * Tests the lookup and getPersonData methods to check if the 
+   * correct data is found and outputted correctly in a string.
+   */
+  public static boolean testGetPersonsData() {
+    return false;
+  }
+  
+  public static void main(String[] args) {
+	  
+  }
+  
+  
+  
+  
+  
+
+
+
+}

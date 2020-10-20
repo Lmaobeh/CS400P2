84171f14ad2e91b661e704c151cdced64af79d35
tnaz84 <71796629+tnaz84@users.noreply.github.com>
2020-10-20 10:18:20 -0500

    Update FriendFinderTester.java

--- FriendFinderTester.java
+++ FriendFinderTester.java
@@ -5,60 +5,130 @@
 // TA: Daniel Finer
 // Lecturer: Florian
 // Notes to Grader: <optional extra notes>
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-public class FriendFinderTester{
+class FriendFinderTester {
 
-  /**
-   * Tests that FriendFinderTree cannot print out an empty tree
-   */
-  public static boolean testIsEmpty() {
-    FriendFinderRBTree test1 = new FriendFinderRBTree;
-    boolean result = test1.isEmpty();
-    if (result != true) {
-    	return false; 
-    }
-    return true;
-  }
-  
-  /**
-   * Tests the contains method to check if the inserted friends are part of the tree
-   */
-  public static boolean testContains() {
-    return false;
-  }
-  
-  /**
-   * Tests the PrintAllInOrder method to check if the correct order of the tree is 
-   * printed out.
-   */
-  public static boolean testPrintAllInOrder() {
-    return false;
-  }
-  
-  /**
-   * Tests the PrintAllLevelOrder method to check ...
-   */
-  public static boolean testPrintAllLevelOrder() {
-    return false;
-  }
-  
-  /**
-   * Tests the lookup and getPersonData methods to check if the 
-   * correct data is found and outputted correctly in a string.
-   */
-  public static boolean testGetPersonsData() {
-    return false;
-  }
-  
-  public static void main(String[] args) {
-	  
-  }
-  
-  
-  
-  
-  
-
-
+	/**
+	   * Tests that checkForMutualFriend method to make sure it works correctly
+	   */
+	@Test
+	void testCheckForMutualFriend() {
+		FriendFinderRBTree taylorTree = new FriendFinderRBTree();
+	    taylorTree.insert("Bryan Waters");
+	    FriendFinderRBTree sherleyTree = new FriendFinderRBTree();
+	    bryanTree.insert("Taylor Towers");
+	    Person taylor = new Person("Taylor Towers", "Lady", "Madison Avenue", taylorTree);
+	    Person bryan = new Person("Bryan Waters", "Dude", "Lakeshore Path", bryanTree);
+	    //Kyle becomes taylors mutual friend
+	    Person kyle = new Person("kyle", "Dude", "Imaginary Avenue", taylorTree);
+	    FriendFinder app = new FriendFinder();
+	    app.masterList.insert(taylor);
+	    app.masterList.insert(bryan);
+	    app.masterList.insert(kyle); 
+	    app.updateCurrentUser("Taylor Towers"); //makes Taylor the current user
+	    if(!app.curUser.checkForMutualFriend(bryan)) {
+	      fail("Taylor and Bryan mutual bestfriends");
+	    }
+	    if(app.curUser.checkForMutualFriend(kyle)) {
+	      fail("Kyle is mutual friens with Taylor");
+	    }
+	    app.updateCurrentUser("Bryan Waters"); //updates the current user to Bryan
+	    if(!app.curUser.checkForMutualFriend(taylor)) {
+	      fail("Bryan and Taylor are mutual bestfriends");
+	    }
+	    if(app.curUser.checkForMutualFriend(kyle)) {
+	      fail("Kyle is not mutual friends with Taylor");
+	    }
+	    app.curUser.insertFriend(kyle);
+	    if(!app.curUser.checkForMutualFriend(kyle)) {
+	      fail("kyle can't be mutual friends with himself");
+	    }
+	    if(app.curUser.checkForMutualFriend(bryan)) {
+	      fail("Kyle is not mutual friends with Bryan, add as friends using inserFriend");
+	    }
+	    app.updateCurrentUser("Taylor Towers"); //makes Taylor the current user
+	    app.curUser.setBio("Woman");
+	    app.curUser.setResidency("Texas");
+	    if(!app.curUser.getBio().equals("Woman")) {
+	      fail("Taylor should be a woman after using setBio()");
+	    }
+	    if(!app.curUser.getResidency().equals("Texas")) {
+	      fail("Taylor should be from Texas after using setResidency() but instead it has: " + app.curUser.getResidency());
+	    }
+	    app.curUser.saveCurrentUser();
+	}
+	
+	/**
+	   * Tests the insert friend method to check if the person is properly added as a mutual friend
+	   *fails if the friend is already added
+	   */
+	@Test
+	void testInsertFriend() { 
+		 FriendFinder app = new FriendFinder();
+		    app.masterList.insert("Tricia"); //adds friend
+		    String result = app.masterList.toString();
+		    if (!app.masterList.toString().equals(result)) {
+		      fail("Friend has not been properly added");
+		    }
+		    app.masterList.insert("Tricia"); //same friend cannot be added twice
+		    String wrongResult = result + result;
+		    if (app.masterList.toString().equals(wrongResult)) {
+		        fail("Tricia has been added to the friend tree twice.");
+		      }
+	}
+		   
+	
+	 /**
+	   * Tests the SaveCurrentUser method to make sure the user is properly saved in the application.
+	   */
+	@Test
+	void testSaveCurrentUser() {
+		FriendFinder app = new FriendFinder();
+	    	Person friend1 = new Person("Taylor", "Lady", "Madison Avenue", null);
+	    	app.masterList.insert(friend1);
+	    	app.saveCurrentUser("Taylor");
+	    	//checks if the current user is saved
+	    	if(!app.curUser.equals(friend1)) {
+	    		fail("Taylor is not saved as the current user");
+	    }
+	    	 app.curUser.setBio("Woman");
+	 	    app.curUser.setResidency("Texas");
+	 	   if(!app.curUser.getBio().equals("Woman")) {
+	 	      fail("Taylor should be a woman after using setBio()");
+	 	    }
+	 	    if(!app.curUser.getResidency().equals("Texas")) {
+	 	      fail("Taylor should be from Texas after using setResidency() but instead it has: " + app.curUser.getResidency());
+	 	    }
+	    	
+	}
+	
+	 /**
+	   * Tests the updateCurrentUser method to check if the user is properly updated from the master list.
+	   */
+	@Test
+	void testUpdateCurrentUser() {
+		FriendFinder app = new FriendFinder();
+			Person friend1 = new Person("Taylor", "Lady", "Madison Avenue", null);
+			app.masterList.insert(friend1);
+			app.updateCurrentUser("Taylor");
+	    	//checks if the current user is updated
+	    	if(!app.curUser.equals(friend1)) {
+		      fail(app.curUser.getFullName() + "is the current user, should be Taylor.");
+		    }
+		    String taylor = app.masterList.toString();
+		    if((!app.masterList.toString().equals(friend1.toString())) || (app.curUser.getFriendsList() != null)){
+		      fail("The friend list doesn't match the person added.");
+		    }
+		    if((!app.curUser.getFullName().equals(friend1.getFullName())) || (!app.curUser.getResidency().equals(friend1.getResidency()))) {
+		      fail("The name or location of the current node does not match Taylor");
+		    }
+		    if(!app.curUser.getBio().equals(friend1.getBio())) {
+		      fail("Incorrect bio for current user, Taylor");
+		    }
+		   
+		    
+	}
 
 }

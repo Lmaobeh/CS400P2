c0fa9ad06ae23a4335bccd00b581a74c80fa53c9
probert206 <71905816+probert206@users.noreply.github.com>
2020-10-20 20:03:23 -0500

    Update FriendFinderFrontEnd.java

--- FriendFinderFrontEnd.java
+++ FriendFinderFrontEnd.java
@@ -8,6 +8,11 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/*
+*This is a driver class that allows a user to access the FriendFinder Applications
+*different users can log in and create their own Red and Black Trees filled with their friends
+*All of these different trees are stored in one master tree
+*/
 public class FriendFinderFrontEnd {
 
 	public static void main(String [] args) {
@@ -28,7 +33,7 @@ public class FriendFinderFrontEnd {
 		}
 		
 
-		
+		//List of commands that user can choose from that will run continuously
 		while(run) {
 	
 		System.out.println("Thanks " + tree.curUser.getFullName() + ", please input a command"); //prints instructions for users
@@ -40,10 +45,13 @@ public class FriendFinderFrontEnd {
 		        + "\nEnter q to end program"
 		        + "\nEnter q! to exit and save");
 		 switch(reader.next()) {
+		//Prints out master tree
 		 case "p": System.out.println(tree.masterList);
 		 break;
+		//Prints out the current users profile
 		 case "v": System.out.println(tree.curUser);
 		 break;
+		//Allows users to change their personal information
 		 case "f": 
 		 boolean run2 = true;
 		 do {
@@ -51,11 +59,13 @@ public class FriendFinderFrontEnd {
 	         System.out.print("Enter r to change residency" +
 	         "\nEnter b to change bio");
 		 switch(reader.next()) {
+		//changes residency
 		 case "r":	System.out.println("Enter new Residency: ");
 		     reader.nextLine();
 			 tree.curUser.setResidency(reader.nextLine());
 			 run2 = false;
 			 break;
+		//changes personal bio
 		 case "b": System.out.println("Enter new Bio ");
 		     reader.nextLine();
 			 tree.curUser.setBio(reader.nextLine());
@@ -67,7 +77,7 @@ public class FriendFinderFrontEnd {
 		   }
 		 } while (run2);
 		   break;
-		   
+		 //allows current user to view a friends profile
 		 case "vf":
 		   System.out.println("Enter the person's name");
 		   reader.nextLine();
@@ -79,21 +89,23 @@ public class FriendFinderFrontEnd {
 		     System.out.println("this person doesn't exist");
 		   }
 		   break;
+		//Allows a user to add a new friends
 		 case "c": System.out.println("Add a new friend: \nWhat is their name?");
 		 reader.nextLine();
 		 String newFriend = reader.nextLine();
 		 if (tree.masterList.contains(newFriend)) {
 		   if (newFriend.equals(tree.curUser.getFullName()) || !tree.curUser.insertFriend(tree.masterList.lookup(newFriend)) ) {
-		     System.out.println(newFriend + " is already your friend");
+		     System.out.println(newFriend + " is already your friend"); //if friend is already added
 		   }
 		 }
+		//if friend does not already have a profile a new profile is created
 		 else {
 		 Person newPerson = new Person(newFriend);
 		 tree.masterList.insert(newPerson);
 		 tree.curUser.insertFriend(newPerson);
 		 }
 		 break;
-		 
+		 //Allows a user to check to see if they and another user are mutual friends
 		 case "x": System.out.println("For what friend would you like to check mutuality?");
 		 reader.nextLine();
 		 Person p = tree.masterList.lookup(reader.nextLine());
@@ -106,14 +118,17 @@ public class FriendFinderFrontEnd {
 			 System.out.println(friend + " and you are mutual friends!");
 		 else {System.out.println("Sadly, " + friend + " and you are not mutual friends.");}
 		 break;
+		//Allows the changing of users
 		 case "u":
 		   System.out.println("Enter the name of the new user: ");
 		   reader.nextLine();
 		   String newUser = reader.nextLine();
 		   tree.updateCurrentUser(newUser);
 		   break;
+		//Lets a user quit the application
 		 case "q": run = false;
 		 break;
+		//Lets the user quit and save the application
 		 case "q!" : run = false;
 		 try {
 		 tree.exitAndSave("FriendFinder.ser");
